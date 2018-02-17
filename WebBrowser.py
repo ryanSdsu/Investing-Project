0c71709eae1cf4f25c568c4708efb29bc7b6f70d
Ryan Ragasa <RJ@ryans-imac.home>
2018-02-17 02:00:49 -0800

    Edited and created WebBrowser

---
+++ WebBrowser.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+from ReturnOnInvestedCapital import ROIC
+
+webbrowser.open('http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB')
+
+
+ROIC("/Users/RJ/Downloads/AAPL Key Ratios.csv.html")
