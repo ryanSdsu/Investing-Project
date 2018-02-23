a9816cbca2e0fb532ee8cebd805022a0f926d6e1
Ryan Ragasa <RJ@ryans-imac.home>
2018-02-23 15:39:33 -0800

    Created a completion method

--- YearAfterYearGrowthSorter/YAYGS_Main_RUNME.py
+++ YearAfterYearGrowthSorter/YAYGS_Main_RUNME.py
@@ -5,3 +5,4 @@ with open("/Users/RJ/PycharmProjects/Investing-Project/YearAfterYearGrowthSorter
     for idx,ticker in enumerate(ROIClist):
         YearOverYearGrowthCheck(ticker)
         print("{} of {} to go!".format(idx, len(ROIClist)))
+        print("Sorting Complete!")
